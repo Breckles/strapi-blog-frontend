59687f961a37269e13e191995deffed76803e290
Waylon <wayloncn@gmail.com>
2021-03-29 10:33:04 -0400

    - made the images in the posts component responsive - made changes to the Formats model

--- src/app/models/image/formats.model.ts
+++ src/app/models/image/formats.model.ts
@@ -1,5 +1,13 @@
 export interface Formats {
   [name: string]: {
+    name: string;
+    hash: string;
+    ext: string;
+    mime: string;
+    width: number;
+    height: number;
+    size: number;
+    path: any;
     url: string;
   };
 }
